456b88636ebbcf485b86c89cda09b33dd52d963b
Yoshika <jsfeeylol@gmail.com>
2026-01-26 21:43:32 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -1,14 +1,33 @@
 package;
 
-import flixel.FlxGame;
+import lime.app.Application;
 import openfl.display.Sprite;
+import flixel.FlxGame;
 import states.TitleState;
 
-class Main extends Sprite
+class Main extends Application
 {
-		public function new()
-		{
-				super();
-				addChild(new FlxGame(0, 0, TitleState));
-		}
+	public function new()
+	{
+		super();
+	}
+
+	override function onWindowCreate():Void
+	{
+		super.onWindowCreate();
+
+		// OpenFL root
+		var root = new Sprite();
+		window.stage.addChild(root);
+
+		// Start Flixel
+		var game = new FlxGame(
+			0, 0,
+			TitleState,
+			60, 60,
+			true
+		);
+
+		root.addChild(game);
+	}
 }
