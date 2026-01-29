b9b9652506bc3a5ffae6d0e6b41c9f664ddae36e
Yoshika <jsfeeylol@gmail.com>
2026-01-29 11:15:41 -0300

    Add GunButton class for shooting functionality

---
+++ source/mobile/objects/buttons/GunButton.hx
@@ -0,0 +1,67 @@
+package mobile.objects.buttons;
+
+#if android
+import flixel.FlxSprite;
+import flixel.FlxG;
+import flixel.math.FlxPoint;
+import objects.Gun;
+import backend.Character;
+
+class GunButton extends FlxSprite
+{
+	public var gun:Gun;
+	public var owner:Character;
+
+	public function new(gun:Gun, owner:Character)
+	{
+		super(514, 208);
+
+		this.gun = gun;
+		this.owner = owner;
+
+		loadGraphic("assets/images/objects/buttons/button_shot.png");
+
+		// Zoom: 66%
+		scale.set(0.66, 0.66);
+		updateHitbox();
+
+		scrollFactor.set();
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+
+		if (owner == null || !owner.exists || !owner.canShoot)
+			return;
+
+		for (touch in FlxG.touches.list)
+		{
+			if (
+				touch.justPressed &&
+				overlapsPoint(
+					FlxPoint.get(touch.screenX, touch.screenY)
+				)
+			)
+			{
+				shoot();
+				break;
+			}
+		}
+	}
+
+	function shoot():Void
+	{
+		// Same behavior as desktop click
+		gun.shoot();
+	}
+
+	override public function destroy():Void
+	{
+		gun = null;
+		owner = null;
+		super.destroy();
+		#end
+	}
+}
+
