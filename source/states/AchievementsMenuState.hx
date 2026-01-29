19d94cea21099ad55b20cc11007e3cd526646350
Yoshika <jsfeeylol@gmail.com>
2026-01-29 11:45:35 -0300

    Add AchievementsUI import to AchievementsMenuState

--- source/states/AchievementsMenuState.hx
+++ source/states/AchievementsMenuState.hx
@@ -2,6 +2,7 @@ package states;
 
 import flixel.FlxState;
 import backend.AchievementsLoader;
+import backend.ui.AchievementsUI;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.text.FlxText;
